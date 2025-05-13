bdaba7ee5650943a89f9f5c3ee6278d9607ab6ba
JojoFlex1 <whateveriffeel@gmail.com>
2025-05-14 01:26:25 +0300

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Downgrade from error to warning
+      "@typescript-eslint/no-unused-vars": "warn",
+      // Downgrade from error to warning
+      "@typescript-eslint/no-explicit-any": "warn",
+      // Downgrade from error to warning
+      "react/no-unescaped-entities": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
