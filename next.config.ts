b10b899fcc997849946a32d539c58d113d1995d4
JojoFlex1 <whateveriffeel@gmail.com>
2025-05-14 08:22:59 +0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // Add ESLint configuration to disable linting during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
-
 
